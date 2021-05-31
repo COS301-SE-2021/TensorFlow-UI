fbedfef3ba811f846dccdf80d2e11ec34015ec40
Felipe Jorge <72206542+FelipeJorge805@users.noreply.github.com>
2021-05-31 20:43:05 +0200

    Springboot project

---
+++ TensorflowUI/src/main/java/com/capstone/TensorflowUI/TensorflowUiApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.TensorflowUI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TensorflowUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TensorflowUiApplication.class, args);
+	}
+
+}
