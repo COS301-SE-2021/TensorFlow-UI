9ece58743718dda8f6bb9ff9377935dfc8d937d2
Wessel Kruger <72206668+Wesselk21@users.noreply.github.com>
2021-06-01 23:57:02 +0200

    Added position field and getter.

--- src/main/java/capstone/TensorflowUI/Node/Model/Node.java
+++ src/main/java/capstone/TensorflowUI/Node/Model/Node.java
@@ -1,6 +1,10 @@
 package capstone.TensorflowUI.Node.Model;
 
 public class Node {
+    private int position;
     public Node(int position) {
+        this.position = position;
     }
+
+    public int getPosition(){return position;}
 }
