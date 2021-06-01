553d26574987b789592a1adf93e798d536f394ad
Wessel Kruger <72206668+Wesselk21@users.noreply.github.com>
2021-06-02 00:09:09 +0200

    Adding functionality to CreateNode UnitTest.

--- src/test/java/capstone/TensorflowUI/TensorflowUiApplicationTests.java
+++ src/test/java/capstone/TensorflowUI/TensorflowUiApplicationTests.java
@@ -3,6 +3,7 @@ package capstone.TensorflowUI;
 import capstone.TensorflowUI.Node.Request.CreateNodeRequest;
 import capstone.TensorflowUI.Node.Response.CreateNodeResponse;
 import capstone.TensorflowUI.Node.Service.NodeServiceImplementation;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -16,7 +17,8 @@ class TensorflowUiApplicationTests {
 
 	@Test
 	void CreateNode(){
-
-
+		System.out.println("Create Node:");
+		CreateNodeResponse Node1 = NodeService.CreateNode(new CreateNodeRequest(0));
+		Assertions.assertEquals(0, Node1.getNode().getPosition());
 	}
 }
