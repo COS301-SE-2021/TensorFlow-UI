86c31f16c589bf2fa401c2d976ab2096d9ef0542
Wessel Kruger <72206668+Wesselk21@users.noreply.github.com>
2021-06-01 23:53:47 +0200

    Creating unitTeas for createNode.

--- src/test/java/capstone/TensorflowUI/TensorflowUiApplicationTests.java
+++ src/test/java/capstone/TensorflowUI/TensorflowUiApplicationTests.java
@@ -1,13 +1,22 @@
 package capstone.TensorflowUI;
 
+import capstone.TensorflowUI.Node.Request.CreateNodeRequest;
+import capstone.TensorflowUI.Node.Response.CreateNodeResponse;
+import capstone.TensorflowUI.Node.Service.NodeServiceImplementation;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TensorflowUiApplicationTests {
 
+	NodeServiceImplementation NodeService = new NodeServiceImplementation();
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void CreateNode(){
+
+
+	}
 }
