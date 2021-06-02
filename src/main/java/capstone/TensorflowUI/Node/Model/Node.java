0e698b29ac11fb235c0e5d45ce64e5af42a1e1df
FelipeJorge805 <feliperkj805gmail.com>
2021-06-02 09:52:40 +0200

    updated node class

--- src/main/java/capstone/TensorflowUI/Node/Model/Node.java
+++ src/main/java/capstone/TensorflowUI/Node/Model/Node.java
@@ -1,9 +1,22 @@
 package capstone.TensorflowUI.Node.Model;
 
+
+import java.util.ArrayList;
+import java.util.UUID;
+
 public class Node {
+    private UUID id;
     private int position;
-    public Node(int position) {
+    private ArrayList<Node> nodes;
+    private Node input;
+    private Node output;
+
+    public Node(int position){
         this.position = position;
+    }
+
+    public void addNode(Node n){
+        nodes.add(n);
     }
 
     public int getPosition(){return position;}
