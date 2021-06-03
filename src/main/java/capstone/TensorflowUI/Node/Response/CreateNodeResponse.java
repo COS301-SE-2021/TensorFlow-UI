1298e7aefe478aa285bd1dea613afd4e5ea4eb3d
Siviwe22 <u18221409@tuks.co.za>
2021-06-03 08:52:02 +0200

    Initialising mock variables in the CreateNodeResponse constructor, to set the default values of Node when creating a new node.

--- src/main/java/capstone/TensorflowUI/Node/Response/CreateNodeResponse.java
+++ src/main/java/capstone/TensorflowUI/Node/Response/CreateNodeResponse.java
@@ -3,10 +3,15 @@ package capstone.TensorflowUI.Node.Response;
 import capstone.TensorflowUI.Node.Model.Node;
 
 public class CreateNodeResponse {
-    private Node node;
+    private final Node node;
 
     public CreateNodeResponse(Node node_) {
         this.node = node_;
+        this.node.setInput("Null");
+        this.node.setOutput("Null");
+        this.node.setAction("[]");
+        this.node.setNodeData("[]");
+        this.node.setType("Something");
     }
 
     public Node getNode() {
