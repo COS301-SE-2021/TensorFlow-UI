8886c5ebfc29ddf0f0113892f87f9d2ff911bef3
Siviwe22 <u18221409@tuks.co.za>
2021-06-03 08:56:14 +0200

    New variables added to Node for testing purposes and variables are converted to Json using jackson.annotation.JsonProperty

--- src/main/java/capstone/TensorflowUI/Node/Model/Node.java
+++ src/main/java/capstone/TensorflowUI/Node/Model/Node.java
@@ -1,10 +1,42 @@
 package capstone.TensorflowUI.Node.Model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.UUID;
+
 public class Node {
-    private int position;
-    public Node(int position) {
-        this.position = position;
+    @JsonProperty ("nodeID")
+    private final UUID nodeID;
+    private String functionName;
+    @JsonProperty("action")
+    private String action;
+    @JsonProperty("type")
+    private String type; //variable - should be a class
+    @JsonProperty("nodeData")
+    private String nodeData;
+    @JsonProperty("input")
+    private String input;
+    @JsonProperty("output")
+    private String output;
+    public Node(UUID nodeID) {
+        this.nodeID = nodeID;
     }
 
-    public int getPosition(){return position;}
+    public UUID getNodeID(){return nodeID;}
+
+    public void setAction(String action){
+        this.action = action;
+    }
+    public void setInput(String input){
+        this.input = input;
+    }
+    public void setOutput(String output){
+        this.output = output;
+    }
+    public void setNodeData(String nodeData){
+        this.nodeData = nodeData;
+    }
+    public void setType(String type){
+        this.type = type;
+    }
 }
