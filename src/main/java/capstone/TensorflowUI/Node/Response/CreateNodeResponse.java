ca492a9049be334be2bd42bf50011927c0da70c9
Siviwe22 <u18221409@tuks.co.za>
2021-06-03 12:07:30 +0200

    Update to new structure

--- src/main/java/capstone/TensorflowUI/Node/Response/CreateNodeResponse.java
+++ src/main/java/capstone/TensorflowUI/Node/Response/CreateNodeResponse.java
@@ -3,15 +3,10 @@ package capstone.TensorflowUI.Node.Response;
 import capstone.TensorflowUI.Node.Model.Node;
 
 public class CreateNodeResponse {
-    private final Node node;
+    private Node node;
 
     public CreateNodeResponse(Node node_) {
         this.node = node_;
-        this.node.setInput("Null");
-        this.node.setOutput("Null");
-        this.node.setAction("[]");
-        this.node.setNodeData("[]");
-        this.node.setType("Something");
     }
 
     public Node getNode() {
