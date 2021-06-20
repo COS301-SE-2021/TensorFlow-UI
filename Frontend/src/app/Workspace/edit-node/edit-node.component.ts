1c7c1eca2a0865af95af27e8479577f8366cc2f5
Siviwe22 <u18221409@tuks.co.za>
2021-06-20 21:33:05 +0200

    creation of update function when update node is clicked

--- Frontend/src/app/Workspace/edit-node/edit-node.component.ts
+++ Frontend/src/app/Workspace/edit-node/edit-node.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit, ViewChild} from '@angular/core';
 import {NodeData} from "../../node-data";
 import {DataService} from "../../data.service";
 import {MatSidenav} from "@angular/material/sidenav";
+import {FormControl} from "@angular/forms";
 
 @Component({
   selector: 'app-edit-node',
@@ -14,19 +15,35 @@ export class EditNodeComponent implements OnInit {
 
   currentNode: NodeData;
   editNodeBool: boolean;
+  outputNode: any;
 
-  constructor(private data: DataService) {
+  nodeName = new FormControl('');
+  nodeType = new FormControl('');
+
+  nodeTypes: String[] = ["Array","Boolean","Integer","String","Void"];
+
+  constructor(public data: DataService) {
 
   }
 
   test(){
     console.log(this.currentNode);
-    console.log(this.data.currentNode);
+    //console.log(this.data.currentNode.name);
   }
 
   ngOnInit(): void {
     this.data.createNodeBoolean.subscribe(editBool => this.editNodeBool = editBool);
     this.data.currentNodeValue.subscribe(currentNode => this.currentNode = currentNode);
+  }
+
+  onClickSubmit(data:any) {
+    if(this.nodeName.value != ""){
+      this.data.currentNode.name = this.nodeName.value;
+    }
+
+    if( this.nodeType.value != ""){
+      this.data.currentNode.type = this.nodeType.value;
+    }
   }
 
 }
