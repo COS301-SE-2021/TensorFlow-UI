2d03f32303bd681059390e6da309aa61e82e1bb4
Siviwe22 <Siviwe22@users.noreply.github.com>
2021-07-10 00:03:49 +0200

    D3: amendment to main d3 module ts file

--- Frontend/src/app/d3/d3.module.ts
+++ Frontend/src/app/d3/d3.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { BarComponent } from './bar/bar.component';
+import { PieComponent } from './pie/pie.component';
+import { ScatterComponent } from './scatter/scatter.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BarComponent,
+    PieComponent,
+    ScatterComponent
+  ],
+  exports: [
+    BarComponent,
+    PieComponent,
+    ScatterComponent
+  ],
   imports: [
     CommonModule
   ]
