1f3a60eeca0895818b93d15f0e9b6a2826efe7c7
FelipeJorge805 <feliperkj805@gmail.com>
2021-08-07 15:23:11 +0200

    added interface, @Injectable, @Selector and proper node data type

--- Frontend/src/app/Workspace/workspace.state.ts
+++ Frontend/src/app/Workspace/workspace.state.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Action, Selector, State, StateContext } from '@ngxs/store';
+import { NodeData} from "../node-data";
 
+export interface WorkspaceStateModel{
+  nodes: NodeData[];
+}
 
-
-@State<CanvasStateModel>({
+@State<WorkspaceStateModel>({
   name: 'canvas',
   defaults: {
-    nodes: [{ name: 'TestNode', width: 500, height: 50, x: 0, y: 0 } as INode],
+    nodes: [{ num: 0, name: 'TestNode', type: "variable", x: 0, y: 0 } as NodeData],
   },
 })
+@Injectable()
+export class workspaceState{
+  @Selector()
+  static nodes(state: WorkspaceStateModel) {
+    return state.nodes;
+  }
+}
