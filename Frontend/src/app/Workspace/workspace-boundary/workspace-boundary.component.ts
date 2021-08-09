51a81a98e23ebbff06e4a862b8ea77b47a65e1b3
wrvrensburg <u15118046@tuks.co.za>
2021-08-09 14:38:55 +0200

    Removal and formatting.

--- Frontend/src/app/Workspace/workspace-boundary/workspace-boundary.component.ts
+++ Frontend/src/app/Workspace/workspace-boundary/workspace-boundary.component.ts
@@ -1,41 +1,19 @@
 import {AfterContentInit, Component, Input, OnInit} from '@angular/core';
 import {DataService} from "../../data.service";
-import {NodeData} from "../../node-data";
-import interact from 'interactjs';
-import * as d3 from 'd3';
 
 @Component({
-  selector: 'app-workspace-boundary',
-  templateUrl: './workspace-boundary.component.html',
-  styleUrls: ['./workspace-boundary.component.css']
+	selector: 'app-workspace-boundary',
+	templateUrl: './workspace-boundary.component.html',
+	styleUrls: ['./workspace-boundary.component.css']
 })
 export class WorkspaceBoundaryComponent implements OnInit {
 
-  createNodeBool: boolean;
-  reloadWorkspace: boolean;
-  @Input() loadWorkspace: boolean;
-  globalLoadWorkspace: boolean =true;
 
-  constructor(public data: DataService) {
-    this.data.createNodeBoolean.subscribe(nodeBool =>this.createNodeBool = nodeBool)
-  }
+	constructor(public data: DataService) {
+	}
 
-  ngOnInit(): void {
-
-    if(this.createNodeBool){
-      console.log("YEss")
-      this.addNodeToWorkspace();
-    }
-
-    this.data.nodes = [];
-
-    d3.select("#canvas")
-      .append("svg")
-      .attr("id","mainSvg")
-      .attr("width", "99%")
-      .attr("height", "100rem ")
-      .style("border","2px solid black")
-      .style("position", "absolute")
-  }
-
+	ngOnInit(): void {
+		this.data.nodes = [];
+		this.data.lineConnectorsList = [];
+	}
 }
