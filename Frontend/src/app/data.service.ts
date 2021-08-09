c5175f2e5a7d19a4771b99ef36348d5a23fa461a
wrvrensburg <u15118046@tuks.co.za>
2021-08-09 14:43:34 +0200

    Removal and formatting.

--- Frontend/src/app/data.service.ts
+++ Frontend/src/app/data.service.ts
@@ -1,80 +1,63 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {BehaviorSubject} from "rxjs";
-import {NodeData} from "./node-data";
 import {lineConnectors, NodeData} from "./node-data";
 
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class DataService {
-  get nodes(): NodeData[] {
-    return this._nodes;
 	get lineConnectorsList(): lineConnectors[] {
 		return this._lineConnectorsList;
 	}
 
-  set nodes(value: NodeData[]) {
-    this._nodes = value;
-  }
-  get type(): string {
-    return this._type;
-  }
 	set lineConnectorsList(value: lineConnectors[]) {
 		this._lineConnectorsList = value;
 	}
 
-  get name(): string {
-    return this._name;
-  }
+	get nodes(): NodeData[] {
+		return this._nodes;
+	}
 
-  set currentNode(node: NodeData){
-    this._currentNode = node;
-  }
+	set nodes(value: NodeData[]) {
+		this._nodes = value;
+	}
 
-  get currentNode(): NodeData{
-    return this._currentNode;
-  }
+	get type(): string {
+		return this._type;
+	}
 
-  set nodeCreationBoolean(create: boolean) {
-    this._nodeCreationBoolean = create;
-  }
+	get name(): string {
+		return this._name;
+	}
 
-  private _name = "";
-  private _type = "";
-  private _x:number=0; //change to private
-  private _y:number=0; //change to private
+	set currentNode(node: NodeData) {
+		this._currentNode = node;
+	}
 
-  private _nodes: NodeData[];
-  private _currentNode: NodeData;
-  private _nodeCreationBoolean: boolean;
+	get currentNode(): NodeData {
+		return this._currentNode;
+	}
 
-  private tempNode: NodeData[] = [];
+	private _name = "";
+	private _type = "";
 
-  private createNodeSource = new BehaviorSubject(false); //default = false
-  private createFormSource = new BehaviorSubject(false); //default = false
+	private _nodes: NodeData[];
+	private _currentNode: NodeData;
+	private _nodeCreationBoolean: boolean;
 	private _lineConnectorsList: lineConnectors[];
 
-  createNodeBoolean = this.createNodeSource.asObservable();
-  createFormBoolean = this.createFormSource.asObservable();
 
-  constructor() {
-  }
+	private createNodeSource = new BehaviorSubject(false); //default = false
 
-  changeCreateNodeBoolean(createNodeSection: boolean) {
-    this.createNodeSource.next(createNodeSection)
-  }
+	createNodeBoolean = this.createNodeSource.asObservable();
 
-  changeCreateFormBoolean(createFormSection: boolean) {
-    this.createFormSource.next(createFormSection)
-  }
+	constructor() {
+	}
 
-  passFormDataToNode(nodeName: string, nodeDataType: string, nodeResult: string){
-    this._name = nodeName;
-    this._type = nodeDataType;
-  }
 
-  applyZoomableBehaviour() {}
-  applyDraggableBehaviour() {}
-  getForceDirectedGraph() {}
+	passFormDataToNode(nodeName: string, nodeDataType: string, nodeResult: string) {
+		this._name = nodeName;
+		this._type = nodeDataType;
+	}
 }
