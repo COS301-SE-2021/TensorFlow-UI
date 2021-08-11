e33aa9a4c447baeac4b40ac3087734b57a1e38fe
W-Kruger <u18014934@tuks.co.za>
2021-08-11 22:45:09 +0200

    Added functionality: Gets all files from library.

--- Frontend/src/app/GITApi.js
+++ Frontend/src/app/GITApi.js
@@ -2,13 +2,15 @@ import {PAT} from './config.js'
 import {user} from './config.js'
 import {mail} from './config.js'
 
+
+
 export default function Commit (Name, Data){
   var myHeaders = new Headers();
   myHeaders.append("Authorization", PAT);
   myHeaders.append("Content-Type", "application/json");
 
   var raw = JSON.stringify({
-    "message": "my commit message",
+    "message": "Project Export From TFUI",
     "committer": {
       "name": user,
       "email": mail
@@ -30,10 +32,35 @@ export default function Commit (Name, Data){
 }
 
 export function GetList(){
-  var lst = {};
-  return lst;
+  var xhr = new XMLHttpRequest();
+  xhr.addEventListener("readystatechange", function() {
+    if(this.readyState === 4) {
+      var json = JSON.parse(this.responseText);
+      var lst = new Array(json.length-1)
+      for (let i = 1; i < json.length; i++) {
+        lst[i-1]= json[i].name;
+      }
+        poplst(lst);
+    }
+  });
+
+  xhr.open("GET", "https://api.github.com/repos/W-Kruger/TFUI-Community-Library/contents");
+  xhr.send();
 }
 
 export function GetData(ID){
 
+}
+
+function poplst(l){
+  var cbox = document.getElementById("TFUIlib");
+  if (cbox != null){
+    cbox.innerHTML = "";
+    for (let i = 0; i < l.length; i++) {
+      var opt = document.createElement('option');
+      opt.value = l[i];
+      opt.innerHTML = l[i];
+      cbox.appendChild(opt);
+    }
+  }
 }
