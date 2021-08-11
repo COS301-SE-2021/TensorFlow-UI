b6093610d224e9765e7ea6aff455d6dd125340ff
W-Kruger <u18014934@tuks.co.za>
2021-08-11 16:55:41 +0200

    New component: Import Button

--- Frontend/src/app/Workspace/workspace.module.ts
+++ Frontend/src/app/Workspace/workspace.module.ts
@@ -21,7 +21,7 @@ import {NodeElementComponent } from './node-element/node-element.component';
 import {DiagramModule} from "@syncfusion/ej2-angular-diagrams";
 import { CanvasComponent } from './canvas/canvas.component';
 import {ExportComponent} from "./export/export.component";
-//import { ImportComponent } from './import/import.component';
+import { ImportComponent } from './import/import.component';
 
 @NgModule({
     imports: [
@@ -42,6 +42,7 @@ import {ExportComponent} from "./export/export.component";
         CreateNodeSectionComponent,
         NodeElementComponent,
         CanvasComponent,
+        ImportComponent,
     ],
     exports: [
         WorkspaceBoundaryComponent,
