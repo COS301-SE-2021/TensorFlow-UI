1407fb1fbd0b3e4dd793e97aa5d8c8596573be98
FelipeJorge805 <feliperkj805@gmail.com>
2021-08-14 15:47:36 +0200

    update linkNodes()
    added functionality to update child  nodes whenever two nodes are linked

--- Frontend/src/app/Node/node.component.ts
+++ Frontend/src/app/Node/node.component.ts
@@ -91,9 +91,10 @@ export class Node implements OnInit {
 
   // Initial linking between two node elements.
   linkNodes() {
-
+    let otherNode = this.nodesArray.value[this.nodesArray.value.length - 1];
     const lineStartName = this.nodeData.name;
-    const lineEndName = this.nodesArray.value[this.nodesArray.value.length - 1].name.toString();
+    const lineEndName = otherNode.name.toString();
+
     const lineObj = new LeaderLine(
         this.document.getElementById(this.nodeData.name),
         this.document.getElementById(this.nodesArray.value[this.nodesArray.value.length - 1].name.toString()), {
@@ -113,7 +114,21 @@ export class Node implements OnInit {
           end: lineEndName,
           line: lineObj,
         }
-    );}
+    );
+
+    //Setting the children of the newly connected nodes
+    let node = this.data.TFOperator.find(function (node) {
+      if (node.name == lineStartName) {
+        return this;
+      }
+    })
+    console.log("Node found from array: " + JSON.stringify(node))
+    if (otherNode.childOne == undefined) {
+      otherNode.childOne = node
+    }else{
+      otherNode.childTwo = node
+    }
+  }
 
   // Redraw lines for each component.
   reload() {
