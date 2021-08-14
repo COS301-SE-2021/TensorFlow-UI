35054c243aa781135fa9428cf3c06b6683d1f82e
FelipeJorge805 <feliperkj805@gmail.com>
2021-08-14 14:59:31 +0200

    updated TFOperator Super call

--- Frontend/src/app/tf/operator/operator.ts
+++ Frontend/src/app/tf/operator/operator.ts
@@ -1,7 +1,10 @@
 import {TFNode} from "../node";
 
 export abstract class TFOperator extends TFNode {
-	protected constructor(public name: String | undefined = undefined) {
-		super(name);
+	protected constructor(public name: String | undefined = undefined,
+                        public childOne: TFNode | undefined = undefined,
+                        public childTwo: TFNode | undefined = undefined
+                        ) {
+		super(name, childOne, childTwo);
 	}
 }
