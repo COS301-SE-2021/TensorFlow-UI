92b8238f6fee05cbb71aba49a41701cc4afda2a0
FelipeJorge805 <feliperkj805@gmail.com>
2021-08-14 14:01:11 +0200

    created recursive function to generate code
    depth first search function that will go through all the nodes and call their code() function

--- Frontend/src/app/tf/graph.ts
+++ Frontend/src/app/tf/graph.ts
@@ -2,4 +2,12 @@ import {TFNode} from "./node";
 
 export class TFGraph {
 	constructor(public root: TFNode | undefined = undefined) {}
+
+  generateCode(current : TFNode){
+	  let stringCode = "";
+	  stringCode = this.generateCode(current.leftChild);
+    stringCode += this.generateCode(current.leftChild);
+    stringCode += current.code();
+    return stringCode;
+  }
 }
