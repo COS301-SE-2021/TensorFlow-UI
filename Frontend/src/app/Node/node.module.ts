ec771f7a5573e15b2214d755981cd31390a33cdd
wrvrensburg <u15118046@tuks.co.za>
2021-08-15 12:02:34 +0200

    Added operator component.

--- Frontend/src/app/Node/node.module.ts
+++ Frontend/src/app/Node/node.module.ts
@@ -3,12 +3,17 @@ import {Node} from "./node.component";
 import {TensorComponent} from "./tensor/tensor.component";
 import {InputComponent} from "./tensor/input/input.component";
 import {DialogComponent} from "./tensor/input/dialog/dialog.component";
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {MatDialogModule} from "@angular/material/dialog";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatButtonModule} from "@angular/material/button";
 import {CommonModule} from "@angular/common";
 import {MatListModule} from "@angular/material/list";
+import {MatInputModule} from "@angular/material/input";
+import {MatCardModule} from "@angular/material/card";
+import {MatTableModule} from "@angular/material/table";
+import { OperatorComponent } from './operator/operator.component';
+import {MatSelectModule} from "@angular/material/select";
 
 
 @NgModule({
@@ -16,7 +21,8 @@ import {MatListModule} from "@angular/material/list";
 		TensorComponent,
 		InputComponent,
 		DialogComponent,
-		Node
+		Node,
+  OperatorComponent
 	],
 	imports: [
 		FormsModule,
@@ -24,13 +30,19 @@ import {MatListModule} from "@angular/material/list";
 		MatFormFieldModule,
 		MatButtonModule,
 		CommonModule,
-		MatListModule
+		MatListModule,
+		MatInputModule,
+		MatCardModule,
+		MatTableModule,
+		ReactiveFormsModule,
+		MatSelectModule
 	],
 	exports: [
 		TensorComponent,
 		InputComponent,
 		DialogComponent,
-		Node
+		Node,
+		OperatorComponent
 	]
 })
 
