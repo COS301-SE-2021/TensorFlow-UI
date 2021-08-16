fbf717213581eba06469abba200bdc4a19f2bc2b
TangerineZA <72995048+TangerineZA@users.noreply.github.com>
2021-08-16 11:45:38 +0200

    Updated code generator to create file from new data structure using graph class

--- Frontend/src/app/code-generator.service.ts
+++ Frontend/src/app/code-generator.service.ts
@@ -1,175 +1,28 @@
 import { Injectable } from '@angular/core';
 import { Node } from './Node/node';
 import {JSONFile} from "@angular/cli/utilities/json-file";
+import {TFGraph} from "./tf";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CodeGeneratorService {
+  // NEW DATA STRUCTURE PY FILE IMPLEMENTATION BELOW HERE
+  generateFile(head : Node) : File {
+    const fs = require("fs");
 
-  codefile : string;
+    const graph : TFGraph = new TFGraph(head);
+    let code : string = graph.generateCode(head);
 
-  constructor() {
-    this.codefile = "";
-  }
-
-  sendToFile(codeLine : string) {
-    this.codefile += "\n";
-    this.codefile += codeLine;
-  }
-
-  generateCode(node : Node) {
-    switch (node.nodeType) {
-      case "variable":
-        return this.generateVariable(node);
-
-      case "function":
-        return this.generateFunctionCall(node);
-
-      default:
-        return "incorrect node type";
-    }
-  }
-  /**
-   * OKAY SO GAME PLAN WITH THIS:
-   * Gotta first declare functions (1)
-   * Then when declared, we're able to call them
-   * So in that case, use the creation flag to determine whether the functions must first be defined before they can be called
-   * And yeah that should fix some things
-   *
-   */
-  generateFunctionCall(node) {
-    let codeline = "";
-    if (!node.children.isEmpty) {
-      for (let i = 0; i < node.children.length; i++) {
-        this.generateCode(node.children[i]);
+    fs.writeFileSync("../Storage/output.py", code, function (err) {
+      if (err) {
+        return console.log("FIle Error");
       }
-    }
-    codeline += node.code;
-    codeline += "(";
-    codeline += this.generateCode(node.input);
-    codeline += ")";
-    this.sendToFile(codeline);
-    return codeline;
+    })
+
+    let file : File = fs.readFile("../Storage/output.py");
+
+    return file;
   }
-
-  generateVariable(node : Node) {
-    if (node.createNodeBool) {
-      let codeline = "";
-      codeline += node.name;
-      codeline += " = ";
-      codeline += node.data;
-      this.sendToFile(codeline);
-      node.createNodeBool = false;
-      return codeline;
-    } else {
-      return node.data;
-    }
-  }
-
-  createVariables(json) {
-    var node = JSON.parse(json);
-    var variableDeclaration : String = "";
-    for (const child in node) {
-      if(child.type.equals("variable")) {
-        var thisDeclaration =
-          child.name + " = " + child.value + '\n';
-        variableDeclaration += thisDeclaration;
-      }
-    }
-    return variableDeclaration;
-  }
-
-  /*
-  createFunctions(json) {
-    var node = JSON.parse(json);
-    var functionDeclaration : String = "";
-    for (const child in node) {
-      if(child.type.equals("function")) {
-        var inputs : String = "";
-        for (const input in child.inputs) {
-          inputs += input + ", ";
-        }
-        inputs = inputs.substring(0, inputs.length - 2);
-        var thisDeclaration =
-          "def " + child.name + "(" + inputs + "):\n\t";
-
-        thisDeclaration += "result = "
-
-        switch (node.functionType) {
-          case "addition":
-
-            for (const input in child.inputs) {
-              thisDeclaration += input.name + " + ";
-            }
-
-            thisDeclaration += "\n\t"
-
-            break;
-          case "subtraction":
-            for (const input in child.inputs) {
-              thisDeclaration += input.name + " - ";
-            }
-
-            thisDeclaration += "\n\t"
-            break;
-
-          case "multiplication":
-            for (const input in child.inputs) {
-              thisDeclaration += input.name + " * ";
-            }
-
-            thisDeclaration += "\n\t"
-            break;
-
-          case "division":
-            for (const input in child.inputs) {
-              thisDeclaration += input.name + "/+ ";
-            }
-
-            thisDeclaration += "\n\t"
-            break;
-        }
-
-        thisDeclaration += "return result\n"
-
-        functionDeclaration += thisDeclaration;
-      }
-    }
-    return functionDeclaration;
-  }
-
-  createCombinedCode(json) {
-    var code = "import tensorflow\n" +
-      "import pandas as pd\n";
-    code += this.createVariables(json);
-    code += this.createFunctions(json);
-    return code;
-  }
-
-   */
-
-  createModel(type : string, numLayers : Number, densityUnits : Array<Number>, activationTypes : Array<string>, trainingData : JSONFile | null) {
-    var code : string = "";
-    var layers : string = "";
-
-    for (let i = 0; i < numLayers; i++) {
-      layers += "model.add(Dense(units=" + densityUnits[i] + ", activation='" + activationTypes[i] + "'))\n";
-    }
-
-    code += ("model = Sequential()\n" +
-      layers +
-      "model.compile(loss='binary_crossentropy', optimizer='sgd', metrics='accuracy')"
-    )
-
-    return code;
-  }
-
-  createLayer() {}
-
-  createCompile() {}
-
-  createTrain() {}
-
 
 }
