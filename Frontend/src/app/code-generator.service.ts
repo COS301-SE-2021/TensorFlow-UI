1095bedfe53228249e6c421646661d59b8603546
TangerineZA <72995048+TangerineZA@users.noreply.github.com>
2021-08-16 12:28:24 +0200

    Added API calls for Docker interface

--- Frontend/src/app/code-generator.service.ts
+++ Frontend/src/app/code-generator.service.ts
@@ -1,14 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Node } from './Node/node';
-import {JSONFile} from "@angular/cli/utilities/json-file";
-import {TFGraph} from "./tf";
+import {TFGraph, TFNode} from "./tf";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CodeGeneratorService {
   // NEW DATA STRUCTURE PY FILE IMPLEMENTATION BELOW HERE
-  generateFile(head : Node) : File {
+  generateFile(head : TFNode) : File {
     const fs = require("fs");
 
     const graph : TFGraph = new TFGraph(head);
@@ -25,4 +23,31 @@ export class CodeGeneratorService {
     return file;
   }
 
+  runfile (file : File, url : string) : string {
+    var savedResponse : string = "";
+
+    var data = "<file contents here>";
+
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function() {
+      if(this.readyState === 4) {
+        console.log(this.responseText);
+        savedResponse = this.responseText;
+      }
+    });
+
+    xhr.open("POST", url);
+    xhr.setRequestHeader("Content-Type", "application/octet-stream");
+
+    xhr.send(data);
+
+    return savedResponse;
+  }
+
+  createAndRun(head : TFNode, url : string) : string {
+    let file : File = this.generateFile(head);
+    return this.runfile(file, url);
+  }
 }
