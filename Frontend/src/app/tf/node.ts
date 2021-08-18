7e241f325c9e57f3f2dfc4dca744d57ae400191c
wrvrensburg <u15118046@tuks.co.za>
2021-08-19 01:46:32 +0200

    Add. Position attributes added.

--- Frontend/src/app/tf/node.ts
+++ Frontend/src/app/tf/node.ts
@@ -2,6 +2,8 @@ export class TFNode {
 	public childOne: TFNode | undefined = undefined;
 	public childTwo: TFNode | undefined = undefined;
 	public selector: string | undefined = undefined;
+	public positionX: number = 0;
+	public positionY: number = 0;
 
 	constructor(public name: string | undefined = undefined,
 				public type: string | undefined = undefined,
