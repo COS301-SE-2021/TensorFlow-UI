49cbc52d5acd4df5d0c964ed428272717c4c5d0e
Siviwe22 <Siviwe22@users.noreply.github.com>
2021-08-18 21:08:46 +0200

    FIx: Re-instating all dialogs inline with testing standards

--- Frontend/src/app/Components/navbar/navbar.component.spec.ts
+++ Frontend/src/app/Components/navbar/navbar.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import {NavbarComponent} from "./navbar.component";
 import {NgxsModule} from "@ngxs/store";
 import {WorkspaceState} from "../../../Storage/workspace";
-import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+import {MAT_DIALOG_DATA, MatDialogModule, MatDialogRef} from "@angular/material/dialog";
 import {MatMenuModule} from "@angular/material/menu";
 import {MAT_SNACK_BAR_DATA, MatSnackBarModule, MatSnackBarRef} from "@angular/material/snack-bar";
 
@@ -14,7 +14,7 @@ describe('NavbarComponent', () => {
     await TestBed.configureTestingModule({
       imports: [NgxsModule.forRoot([
         WorkspaceState
-      ]),MatMenuModule, MatSnackBarModule],
+      ]),MatMenuModule, MatSnackBarModule, MatDialogModule],
       declarations: [ NavbarComponent ],
       providers: [
         {provide: MAT_DIALOG_DATA, useValue: {}},
--- Frontend/src/app/Components/navbar/navbar.component.ts
+++ Frontend/src/app/Components/navbar/navbar.component.ts
@@ -58,9 +58,8 @@ export class NavbarComponent implements OnInit, AfterViewInit {
 	public functionsList: string[] = ["add", "subtract", "multiply", "divide"];
 	public tensorList: string[] = ["variable", "constant", "tensor"];
 
-
-
-	constructor(private data: DataService, @Inject(DOCUMENT) private document, private store: Store, private snackBar: MatSnackBar) {
+	constructor(private data: DataService, @Inject(DOCUMENT) private document, private store: Store, private snackBar: MatSnackBar,
+              private dialog: MatDialog) {
 	}
 
 	ngOnInit(): void {
@@ -165,8 +164,6 @@ export class NavbarComponent implements OnInit, AfterViewInit {
 	// 	);
 	// }
 
-
-
 	// This adds a new node to the data service "nodes" array.
 	createNode(type: string) {
 		const nodeNum = this.data.nodes.length + 1;
@@ -216,12 +213,12 @@ export class NavbarComponent implements OnInit, AfterViewInit {
 	}
 
 	clearCanvas() {
-		// const clearDialog = this.dialog.open(NavbarDialogsComponent);
+		const clearDialog = this.dialog.open(NavbarDialogsComponent);
 
-		// clearDialog.afterClosed().subscribe(result => {
-		// 	const clearCanvasBoolean = clearDialog.disableClose;
-    //
-		// 	if (clearCanvasBoolean) {
+		clearDialog.afterClosed().subscribe(result => {
+			const clearCanvasBoolean = clearDialog.disableClose;
+
+			if (clearCanvasBoolean) {
 				this.data.nodes.forEach(element => this.store.dispatch(new RemoveNodeFromStorage(element.name)))
 				this.data.lineConnectorsList.forEach(element => this.store.dispatch(new RemoveLineFromStorage(element)))
 				this.data.lineConnectorsList.forEach(element => element.line?.remove())
@@ -232,41 +229,41 @@ export class NavbarComponent implements OnInit, AfterViewInit {
         //new data structure
         this.TFNodeList.forEach(element => this.store.dispatch(new RemoveTFNode(element)))
         this.TFNodeList.splice(0,this.TFNodeList.length)
-		// 	}
-		// })
+			}
+		})
 	}
 
 	showProjectDetails() {
-		// const projectDetailsDialog = this.dialog.open(SettingsPageDialogComponent,
-		// 	{
-		// 		disableClose: true,
-		// 		data: {projectName: this.projectName, projectDetails: this.projectDetails}
-		// 	}
-		// );
+		const projectDetailsDialog = this.dialog.open(SettingsPageDialogComponent,
+			{
+				disableClose: true,
+				data: {projectName: this.projectName, projectDetails: this.projectDetails}
+			}
+		);
 
-		// projectDetailsDialog.afterClosed().subscribe(result => {
-		// 	const detailsAdded = projectDetailsDialog.disableClose;
-    //
-		// 	if (detailsAdded) {
-		// 		//Add to details to ngxs storage and display snackbar
-		// 		const dialogData = projectDetailsDialog.componentInstance;
-		// 		let dataOK: boolean = false;
-		// 		if (dialogData.projectName != undefined && dialogData.projectDescription != undefined) {
-		// 			dataOK = true;
-		// 			this.store.dispatch(new AddProjectName(dialogData.projectName));
-    //       this.store.dispatch(new AddProjectDescription(dialogData.projectDescription));
-		// 		}
-		// 		this.projectDetailsUpdatedSnackbar(dataOK);
-		// 	}
-		// })
+		projectDetailsDialog.afterClosed().subscribe(result => {
+			const detailsAdded = projectDetailsDialog.disableClose;
+
+			if (detailsAdded) {
+				//Add to details to ngxs storage and display snackbar
+				const dialogData = projectDetailsDialog.componentInstance;
+				let dataOK: boolean = false;
+				if (dialogData.projectName != undefined && dialogData.projectDescription != undefined) {
+					dataOK = true;
+					this.store.dispatch(new AddProjectName(dialogData.projectName));
+          this.store.dispatch(new AddProjectDescription(dialogData.projectDescription));
+				}
+				this.projectDetailsUpdatedSnackbar(dataOK);
+			}
+		})
 	}
 
 	projectDetailsUpdatedSnackbar(dataOk: boolean) {
-		// let snackBarRef = this.snackBar.openFromComponent(ProjectDetailsUpdatedSnackbarComponent,
-		// 	{
-		// 		duration: 1000,
-		// 		data: dataOk
-		// 	})
+		let snackBarRef = this.snackBar.openFromComponent(ProjectDetailsUpdatedSnackbarComponent,
+			{
+				duration: 1000,
+				data: dataOk
+			})
 	}
 
 	@ViewChild('sidenav') sidenav: MatSidenav;
