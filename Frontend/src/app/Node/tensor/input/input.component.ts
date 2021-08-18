cbe119127cc40da8438cc6a5d7acaaa9aff0b5f9
wrvrensburg <u15118046@tuks.co.za>
2021-08-18 22:17:32 +0200

    Update. Added input node.

--- Frontend/src/app/Node/tensor/input/input.component.ts
+++ Frontend/src/app/Node/tensor/input/input.component.ts
@@ -1,35 +1,38 @@
-import {Component, Inject, Input, OnInit} from '@angular/core';
-import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from "@angular/material/dialog";
+import {Component, Input, OnInit} from '@angular/core';
+import {MatDialog} from "@angular/material/dialog";
 import {DialogComponent} from "./dialog/dialog.component";
+import {TFNode} from "../../../tf";
 
 export interface DialogData {
-  inputData: string[];
-  inputCount: number;
+	inputData: string[];
+	inputCount: number;
 }
 
 @Component({
-  selector: 'app-input',
-  templateUrl: './input.component.html',
-  styleUrls: ['./input.component.css']
+	selector: 'app-input',
+	templateUrl: './input.component.html',
+	styleUrls: ['./input.component.css']
 })
 export class InputComponent implements OnInit {
-  input : string;
+	input: string;
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
+	@Input() TFNodeData : TFNode;
 
-  inputData: string;
+	inputData: string;
 
-  constructor(public dialog: MatDialog) {}
+	constructor(public dialog: MatDialog) {
+	}
 
-  openDialog(): void {
-    const dialogRef = this.dialog.open(DialogComponent, {
-      width: '250px',
-      data: {inputData: this.inputData}
-    });
+	openDialog(): void {
+		const dialogRef = this.dialog.open(DialogComponent, {
+			width: '250px',
+			data: {inputData: this.inputData}
+		});
 
-    dialogRef.afterClosed().subscribe(inputData => {
-        this.inputData = inputData
-    });
-  }
+		dialogRef.afterClosed().subscribe(inputData => {
+			this.inputData = inputData
+		});
+	}
 }
