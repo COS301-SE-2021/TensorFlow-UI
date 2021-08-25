fadd942bd2ac132dedc980a215b767e7e38c0eb1
wrvrensburg <u15118046@tuks.co.za>
2021-08-25 10:39:45 +0200

    Update. Added import.

--- Frontend/src/app/tf/tensor/transformations/TFSpaceToBatchND.ts
+++ Frontend/src/app/tf/tensor/transformations/TFSpaceToBatchND.ts
@@ -1,4 +1,6 @@
 // tf.spaceToBatchND (x, blockShape, paddings)
+import {TFTensor} from "../tensor";
+
 export class TFSpaceToBatchND extends TFTensor {
 	constructor(public data: number | undefined = undefined,
 				public name: string | undefined = undefined) {
