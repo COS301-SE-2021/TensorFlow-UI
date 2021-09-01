8cdffed43402fc1e6824ad543cae4ae424e66509
wrvrensburg <u15118046@tuks.co.za>
2021-09-01 08:06:20 +0200

    Refactoring. UI structure is set in each class.
    UI structure of each class is now set inside each class and not in the Navbar component.

--- Frontend/src/app/Components/navbar/navbar.component.ts
+++ Frontend/src/app/Components/navbar/navbar.component.ts
@@ -27,18 +27,7 @@ import {CodeGeneratorService} from "../../code-generator.service";
 import {MatDialog} from "@angular/material/dialog";
 import {MatSnackBar, MatSnackBarModule} from "@angular/material/snack-bar";
 import {ProjectDetailsUpdatedSnackbarComponent} from "../project-details-updated-snackbar/project-details-updated-snackbar.component";
-import {
-	TFAdd,
-	TFAddN,
-	TFConstant, TFDivide,
-	TFFill,
-	TFLinespace, TFMod, TFMultiply, TFNegative, TFNode,
-	TFOnes,
-	TFOperator, TFReciprocal, TFScalarMul, TFSigmoid, TFSubtract,
-	TFTensor,
-	TFVariable,
-	TFZeros
-} from "../../tf";
+import {newNode, operatorMath, TFNode} from "../../tf";
 import {SettingsPageDialogComponent} from "../settings-page-dialog/settings-page-dialog.component";
 import {NavbarDialogsComponent} from "../navbar-dialogs/navbar-dialogs.component";
 import * as litegraph from "litegraph.js";
@@ -55,7 +44,7 @@ export interface SettingsPageData {
 	templateUrl: './navbar.component.html',
 	styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit, AfterViewInit{
+export class NavbarComponent implements OnInit, AfterViewInit {
 
 	public TFNodeList: TFNode[] = [];
 	public linesList: lineConnectors[] = [];
@@ -66,6 +55,7 @@ export class NavbarComponent implements OnInit, AfterViewInit{
 	public lines;
 
 	tftensor: string[] = ["Constant", "Variable", "Fill", "Linspace", "Zeros", "Ones"];
+	_operatorMath: string[] = operatorMath;
 	tfoperator: string[] = ["Add", "Add_n", "Divide", "Mod", "Negative", "Reciprocal", "Scalar Multiplication", "Sigmoid", "Subtract", "Multiply"];
 	// TFList: string[] = ["Constant", "Variable", "Fill", "Linespace", "Zeros", "Ones", "Add", "Add_n", "Divide", "Mod", "Negative", "Reciprocal", "Scalar Multiplication", "Sigmoid", "Subtract", "Multiply"];
 
@@ -101,11 +91,12 @@ export class NavbarComponent implements OnInit, AfterViewInit{
 
 	ngAfterViewInit() {
 		const storedNodes = this.store.selectSnapshot(WorkspaceState).TFNode;
-		if(storedNodes.length>0){
+		if (storedNodes.length > 0) {
 			//recreate all these nodes;
 			// console.log(storedNodes);
-			for(let i=0; i<storedNodes.length;++i){
-				this.createLiteNode(storedNodes[i].selector,true,storedNodes[i]);
+			for (let i = 0; i < storedNodes.length; ++i) {
+				//add type nodeObject
+				this.createLiteNode(storedNodes[i].selector, true, storedNodes[i], storedNodes[i]);
 			}
 		}
 	}
@@ -162,12 +153,12 @@ export class NavbarComponent implements OnInit, AfterViewInit{
 				//Add to details to ngxs storage and display snackbar
 				const dialogData = projectDetailsDialog.componentInstance;
 				let dataOK: boolean = false;
-				if ((dialogData.projectName != undefined && dialogData.projectName!="" && dialogData.projectName.match(/^ *$/) == null) && dialogData.projectDescription != undefined) {
+				if ((dialogData.projectName != undefined && dialogData.projectName != "" && dialogData.projectName.match(/^ *$/) == null) && dialogData.projectDescription != undefined) {
 					dataOK = true;
 					this.store.dispatch(new AddProjectName(dialogData.projectName));
 					this.store.dispatch(new AddProjectDescription(dialogData.projectDescription));
 				}
-				console.log("|"+dialogData.projectName+"|");
+				console.log("|" + dialogData.projectName + "|");
 				console.log(dialogData.projectDescription);
 				this.projectDetailsUpdatedSnackbar(dataOK);
 			}
@@ -210,48 +201,47 @@ export class NavbarComponent implements OnInit, AfterViewInit{
 		this.TFNodeList.push(node);
 	}
 
-	createLiteNode(component:string, loadFromMemory: boolean, storedNode:TFNode): LGraphNode {
+	//add type TFnode object
+	createLiteNode(component: string, loadFromMemory: boolean, storedNode: TFNode, tfnode: TFNode): LGraphNode {
 		const node = new litegraph.LGraphNode();
 
-		if(!loadFromMemory) {
+		if (!loadFromMemory) {
 			node.title = component;
 			node.pos = [200, 200]; //ToDo: change this to be dynamic
 			const that = this;
-			node.onMouseDown = function (){
+			node.onMouseDown = function () {
 				const that2 = that;
-				node.onMouseLeave = function (){
+				node.onMouseLeave = function () {
 					that2.updateNodePositionInLocalStorage();
 				}
 			}
-			node.onMouseEnter = function (){
+			node.onMouseEnter = function () {
 				that.updateNodeLinks();
 			}
-			if (this.tftensor.includes(component)) {
-				this.insertTensorData(node, component);
-			} else {
-				this.insertOperatorData(node, component);
-			}
+			tfnode.UIStructure(node);
+
 			this.graph.add(node);
 			this.graph.start();
-		}
-		else{
+		} else {
 			node.title = component;
 			node.pos = storedNode.position;
 			const that = this;
-			node.onMouseDown = function (){
+			node.onMouseDown = function () {
 				const that2 = that;
-				node.onMouseLeave = function (){
+				node.onMouseLeave = function () {
 					that2.updateNodePositionInLocalStorage();
 				}
 			}
-			node.onMouseEnter = function (){
+			node.onMouseEnter = function () {
 				that.updateNodeLinks();
 			}
-			if (this.tftensor.includes(<string>storedNode.selector)) {
-				this.insertTensorData(node,<string>storedNode.selector);
-			} else {
-				this.insertOperatorData(node, <string>storedNode.selector);
-			}
+			// A temporary node is created to get the structure of the UI structure of the object that has been stored in the state.
+			let temp = newNode(<string>storedNode.selector)
+
+			temp.UIStructure(node);
+			// The stored node objects do not have UI components.
+			// storedNode.UIStructure(node);
+
 			this.graph.add(node);
 			this.graph.start();
 		}
@@ -260,232 +250,33 @@ export class NavbarComponent implements OnInit, AfterViewInit{
 
 	createComponent(component: string) {
 
-		const liteGraphNode = this.createLiteNode(component,false,new TFNode());
-		const links = this.graph.list_of_graphcanvas[0].graph.links;
-		console.log(liteGraphNode);
-		console.log(this.graph);
-		console.log(links);
 
 		let tfnode: TFNode;
 		let id: string = Math.random().toString(36).substr(2, 9);
-		switch (component) {
-			case this.tftensor[0]: {
-				tfnode = new TFConstant();
-				tfnode.name = component + id;
-				this.createComponentSwitchDefaults(tfnode,liteGraphNode,component);
-				break;
-			}
-			case this.tftensor[1]: {
-				tfnode = new TFVariable();
-				tfnode.name = component + id;
-				this.createComponentSwitchDefaults(tfnode,liteGraphNode,component);
-				break;
-			}
-			case this.tftensor[2]: {
-				tfnode = new TFFill();
-				tfnode.name = component + id;
-				this.createComponentSwitchDefaults(tfnode,liteGraphNode,component);
-				break;
-			}
-			case this.tftensor[3]: {
-				tfnode = new TFLinespace();
-				tfnode.name = component + id;
-				this.createComponentSwitchDefaults(tfnode,liteGraphNode,component);
-				break;
-			}
-			case this.tftensor[4]: {
-				tfnode = new TFZeros();
-				tfnode.name = component + id;
-				this.createComponentSwitchDefaults(tfnode,liteGraphNode,component);
-				break;
-			}
-			case this.tftensor[5]: {
-				tfnode = new TFOnes();
-				tfnode.name = component + id;
-				this.createComponentSwitchDefaults(tfnode,liteGraphNode,component);
-				break;
-			}
-			case this.tfoperator[0]: {
-				tfnode = new TFAdd();
-				tfnode.name = component + id;
-				this.createComponentSwitchDefaults(tfnode,liteGraphNode,component);
-				break;
-			}
-			case this.tfoperator[1]: {
-				tfnode = new TFAddN();
-				tfnode.name = component + id;
-				this.createComponentSwitchDefaults(tfnode,liteGraphNode,component);
-				break;
-			}
-			case this.tfoperator[2]: {
-				tfnode = new TFDivide();
-				tfnode.name = component + id;
-				this.createComponentSwitchDefaults(tfnode,liteGraphNode,component);
-				break;
-			}
-			case this.tfoperator[3]: {
-				tfnode = new TFMod();
-				tfnode.name = component + id;
-				this.createComponentSwitchDefaults(tfnode,liteGraphNode,component);
-				break;
-			}
-			case this.tfoperator[4]: {
-				tfnode = new TFNegative();
-				tfnode.name = component + id;
-				this.createComponentSwitchDefaults(tfnode,liteGraphNode,component);
-				break;
-			}
-			case this.tfoperator[5]: {
-				tfnode = new TFReciprocal();
-				tfnode.name = component + id;
-				this.createComponentSwitchDefaults(tfnode,liteGraphNode,component);
-				break;
-			}
-			case this.tfoperator[6]: {
-				tfnode = new TFScalarMul();
-				tfnode.name = component + id;
-				this.createComponentSwitchDefaults(tfnode,liteGraphNode,component);
-				break;
-			}
-			case this.tfoperator[7]: {
-				tfnode = new TFSigmoid();
-				tfnode.name = component + id;
-				this.createComponentSwitchDefaults(tfnode,liteGraphNode,component);
-				break;
-			}
-			case this.tfoperator[8]: {
-				tfnode = new TFSubtract();
-				tfnode.name = component + id;
-				this.createComponentSwitchDefaults(tfnode,liteGraphNode,component);
-				break;
-			}
-			case this.tfoperator[9]: {
-				tfnode = new TFMultiply();
-				tfnode.name = component + id;
-				this.createComponentSwitchDefaults(tfnode,liteGraphNode,component);
-				break;
-			}
-			default: {
-				break;
-			}
-		}
+
+		tfnode = newNode(component);
+		tfnode.name = component + id;
+		const liteGraphNode = this.createLiteNode(component, false, new TFNode(), tfnode);
+		const links = this.graph.list_of_graphcanvas[0].graph.links;
+		this.createComponentSwitchDefaults(tfnode, liteGraphNode, component);
+
+		// console.log(liteGraphNode);
+		// console.log(this.graph);
+		// console.log(links);
 	}
 
 	//Sets all values which are the same across every switch statement
-	createComponentSwitchDefaults(node: TFNode,liteGraphNode: LGraphNode, component:string){
+	createComponentSwitchDefaults(node: TFNode, liteGraphNode: LGraphNode, component: string) {
 		node.selector = component;
 		node.id = liteGraphNode.id;
 		node.position = liteGraphNode.pos;
 		this.addNewNode(node);
 	}
 
-	insertTensorData(node: LGraphNode, component: string){
-		switch (component){
-			case "Variable":{
-				// node.addWidget("button", "initialValue", "tf.Tensor","variableName");
-				node.addWidget("toggle","trainable(optional)",false,"onDeselected",{values: [true,false]})
-				node.addWidget("text","name(optional)","uniqueID","variableID");
-				node.addWidget("combo","dtype(optional)","float","variableDType",{values: ["float32","int32","bool","complex64","string"]});
-				node.addInput("tf.Tensor","Tensor");
-				node.addOutput("Variable","tf.Tensor");
 
-				//ToDo: Change how input is viewed
-				break;
-			}
-			case "Constant":{
-				node.addWidget("number","constant",0,"constant");
-				node.addOutput("Value","tf.Tensor")
-				break;
-			}
-			case "Fill":{
-				node.addWidget("text","shape","[0,4,2]","fillShape");
-				node.addWidget("text","value",0,"fillShape");
-				node.addWidget("combo","dtype(optional)","float","fillDType",{values: ["float32","int32","bool","complex64","string"]});
-				node.addOutput("Fill","tf.Tensor")
-				//Todo: Change default width
-				break;
-			}
-			case "Linspace":{
-				node.addWidget("number","start",0,"linspaceStart");
-				node.addWidget("number","stop",0,"linspaceStop");
-				node.addWidget("number","num",1,"linspaceNum");
-				node.addOutput("linspace sequence","tf.Tensor")
-				break;
-			}
-			case "Zeros":{
-				node.addWidget("text","shape","[0,2,4]","zerosShape");
-				node.addWidget("combo","dtype(optional)","float","zerosDType",{values: ["float32","int32","bool","complex64","string"]});
-				node.addOutput("Tensor zeros","tf.Tensor")
-				break;
-			}
-			case "Ones":{
-				node.addWidget("text","shape","[0,2,4]","onesShape");
-				node.addWidget("combo","dtype(optional)","float","zerosDType",{values: ["float32","int32","bool","complex64","string"]});
-				node.addOutput("Tensor ones","tf.Tensor")
-				break;
-			}
-
-		}
-	}
-
-	insertOperatorData(node: LGraphNode, component: string){
-		switch (component) {
-			case "Add":{
-				node.addInput("A","tf.Tensor"); //should be tf.Tensor|TypedArray|Array
-				node.addInput("B","tf.Tensor"); //should be tf.Tensor|TypedArray|Array
-				node.addOutput("A+B","tf.Tensor");
-				break;
-			}
-			case "Add_n":{
-				node.addInput("tensors(Array)","Array"); //ToDo: Ensure this array can receive a sample array, must be same shape and dtype
-				node.addOutput("Tensor list","tf.Tensor");
-				break;
-			}
-			case "Divide":{
-				node.addInput("A","tf.Tensor");
-				node.addInput("B","tf.Tensor");
-				node.addOutput("A/B","tf.Tensor");
-				break;
-			}
-			case "Multiply":{
-				node.addInput("A","tf.Tensor");
-				node.addInput("B","tf.Tensor");
-				node.addOutput("A*B","tf.Tensor");
-				break;
-			}
-			case "Mod":{
-				node.addInput("A","tf.Tensor");
-				node.addInput("B","tf.Tensor");
-				node.addOutput("A%B","tf.Tensor")
-				break;
-			}
-			case "Negative":{
-				node.addInput("A","tf.Tensor");
-				node.addOutput("-(A)","tf.Tensor");
-				break;
-			}
-			case "Reciprocal":{
-				node.addInput("X","tf.Tensor");
-				node.addOutput("1/X","tf.Tensor");
-				break;
-			}
-			case "Sigmoid":{
-				node.addInput("X","tf.Tensor");
-				node.addOutput("1/1+exp(-x)", "tf.Tensor");
-				break;
-			}
-			case "Subtract":{
-				node.addInput("A","tf.Tensor"); //should be tf.Tensor|TypedArray|Array
-				node.addInput("B","tf.Tensor"); //should be tf.Tensor|TypedArray|Array
-				node.addOutput("A-B","tf.Tensor");
-				break;
-			}
-		}
-	}
-
-	updateNodePositionInLocalStorage(){
+	updateNodePositionInLocalStorage() {
 		const selectedNodes = this.graph.list_of_graphcanvas[0].selected_nodes;
-		for(let key in selectedNodes){
+		for (let key in selectedNodes) {
 			const node = selectedNodes[key];
 			const nodeID = node.id;
 			const storedNodesArray = this.store.selectSnapshot(WorkspaceState).TFNode;
@@ -498,9 +289,9 @@ export class NavbarComponent implements OnInit, AfterViewInit{
 		}
 	}
 
-	updateNodeLinks(){
+	updateNodeLinks() {
 
-		for(let key in this.lines){
+		for (let key in this.lines) {
 			const line = this.lines[key];
 
 			const lineObj: lineConnectors = {
@@ -516,12 +307,13 @@ export class NavbarComponent implements OnInit, AfterViewInit{
 		}
 	}
 
-	isAnyInputConnected(node): boolean{
-		for(let i=0; i<node.inputs.length; ++i){
-			if(node.inputs[i].link != null){
+	isAnyInputConnected(node): boolean {
+		for (let i = 0; i < node.inputs.length; ++i) {
+			if (node.inputs[i].link != null) {
 				return true;
 			}
 		}
 		return false;
 	}
 }
+
