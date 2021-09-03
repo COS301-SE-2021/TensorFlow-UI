630dc3131fc1b4828843dbf786cb92dfd6bdc921
wrvrensburg <u15118046@tuks.co.za>
2021-09-03 12:47:19 +0200

    Added. Inputs array to TFNode class.

--- Frontend/src/app/tf/node.ts
+++ Frontend/src/app/tf/node.ts
@@ -4,6 +4,7 @@ import {LGraphNode, Vector2} from "litegraph.js";
 export class TFNode {
 	public childOne: TFNode | undefined = undefined;
 	public childTwo: TFNode | undefined = undefined;
+	public inputs: TFNode[] | undefined = undefined;
 	public selector: string | undefined = undefined;
 	public id:number;
 	public position: Vector2 = [0,0];
