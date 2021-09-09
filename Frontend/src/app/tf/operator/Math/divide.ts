e55bedb112905b5cde22665e4e7c4f9bee0f4391
Siviwe22 <Siviwe22@users.noreply.github.com>
2021-09-09 09:36:54 +0200

    Update: Updates to the code() function of divide and multiply
     - Both code functions for classes updated and now fully implemented

--- Frontend/src/app/tf/operator/Math/divide.ts
+++ Frontend/src/app/tf/operator/Math/divide.ts
@@ -2,33 +2,64 @@ import {TFOperator} from "../operator";
 import {LGraphNode} from "litegraph.js";
 import {Store} from "@ngxs/store";
 import {WorkspaceState} from "../../../../Storage/workspace";
+import {TFNode} from "../../node";
 
 export class TFDivide extends TFOperator {
 	constructor(public name: string | undefined = undefined,private store: Store) {
 		super(name);
 	}
 
-	code() {
+	code(storageLinks,storageNodes) {
 
 		let param1: string = "1";
 		let param2: string = "1";
-		const links = this.store.selectSnapshot(WorkspaceState).links;
-		const nodes = this.store.selectSnapshot(WorkspaceState).TFNode;
+
+		let param1DType: string = "float";
+		let param2DType: string = "float";
+		let denominatorValue: string = param2;
+		let errorInputArray: TFNode[] = [];
 
 		for(let i=0; i<this.inputs.length; ++i){
 			let input = this.inputs[i];
 			if(input.link!=null){
 
-				const link = links.find(element => element.id ==input.link);
-				const inputNode = nodes.find(element => element.id == link.origin_id);
+				const link = storageLinks.find(element => element.id ==input.link);
+				const inputNode = storageNodes.find(element => element.id == link.origin_id);
 
-				if(i==0)
+				if(i==0) {
 					param1 = inputNode.name;
-				else
+					param1DType = this.getDType(inputNode.widgets);
+				}
+				else {
 					param2 = inputNode.name;
+					denominatorValue = "0";
+					errorInputArray.push(inputNode);
+
+					for(let widget of inputNode.widgets){
+						//Check if 2nd input is the same as the first
+						if(widget.type === "dtype"){
+							param2DType = widget.value;
+						}
+						if(widget.type === "constant"){
+							denominatorValue = widget.value;
+						}
+					}
+				}
 			}
 		}
 
+		if(param1DType !== param2DType){
+			//TODO - THROW ERROR
+			let errorMessage = "The second tensor input(B), which is a denominator, must have the same dtype as the first tensor input(A)";
+
+			console.log(errorMessage);
+		}
+		if(denominatorValue === "0"){
+			//TODO - THROW ERROR
+			//Maybe highlight the node
+			let errorMessage = "Division by zero is not allowed";
+			console.log(errorMessage);
+		}
 		return `${this.name} = tf.math.divide(${param1},${param2})`;
 	}
 
--- Frontend/src/app/tf/operator/Math/multiply.ts
+++ Frontend/src/app/tf/operator/Math/multiply.ts
@@ -7,12 +7,40 @@ export class TFMultiply extends TFOperator {
 		super(name);
 	}
 
-	code() {
-		return `${this.name} = tf.math.multiply(
-			${this.TFChildInputs?.forEach(function (key) {
-			key?.name + "," || `some value,`
-		})
-		})`;
+	code(storageLinks,storageNodes) {
+
+		let param1: string = "1";
+		let param2: string = "1";
+		let param1DType: string = "float";
+		let param2DType: string = "float";
+
+		for(let i=0; i<this.inputs.length; ++i){
+			let input = this.inputs[i];
+			if(input.link!=null){
+
+				const link = storageLinks.find(element => element.id ==input.link);
+				const inputNode = storageNodes.find(element => element.id == link.origin_id);
+
+				if(i==0) {
+					param1 = inputNode.name;
+					param1DType = this.getDType(inputNode.widgets);
+
+				}
+				else {
+					param2 = inputNode.name;
+					param2DType = this.getDType(inputNode.widgets);
+
+				}
+			}
+		}
+
+		if(param1DType !== param2DType){
+			//TODO - THROW ERROR
+			let errorMessage = "The second tensor input(B) to multiply, must have the same dtype as the first tensor input(A)";
+			console.log(errorMessage);
+		}
+
+		return `${this.name} = tf.math.multiply(${param1},${param2})`;
 	}
 
 	UIStructure(node: LGraphNode) {
