010d70b1199bba1aa52c11ca4983ed6160ebf464
FelipeJorge805 <feliperkj805@gmail.com>
2021-09-12 12:29:34 +0200

    Update: updated RunCodeCommand class with new functionality

--- Frontend/src/Command/RunCodeCommand.ts
+++ Frontend/src/Command/RunCodeCommand.ts
@@ -1,13 +1,20 @@
 import {Command} from "./Command";
 import {Store} from "@ngxs/store";
+import {CodeGeneratorService} from "../app/code-generator.service";
+import {WorkspaceState} from "../Storage/workspace";
+import {NavbarComponent} from "../app/Components/navbar/navbar.component";
+import {CommandHistory} from "./CommandHistory";
 
 export class RunCodeCommand extends Command{
-
-  constructor(store: Store) {
+  private nav: NavbarComponent
+  constructor(store: Store,navbar: NavbarComponent) {
     super(store);
+    this.nav = navbar;
   }
 
   execute() {
+    const generator : CodeGeneratorService = new CodeGeneratorService(this.store);
+    generator.runFile(this.store.selectSnapshot(WorkspaceState).rootNode,this.nav.TFNodeList,this.store.selectSnapshot(WorkspaceState).links, "localhost:5000");
   }
 
   undo() {
