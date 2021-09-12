e19cacad36e50ce4ac9372fa0007b1e89e5e6807
Siviwe22 <Siviwe22@users.noreply.github.com>
2021-09-12 21:12:34 +0200

    Update: Full implementation of the TFRange and ZerosLike classes

--- Frontend/src/app/tf/tensor/creation/zeroslike.ts
+++ Frontend/src/app/tf/tensor/creation/zeroslike.ts
@@ -1,1 +1,29 @@
-// ???? not sure what is happening here, might have forgoten to fill this in.
+import {TFTensor} from "../tensor";
+import {LGraphNode} from "litegraph.js";
+
+export class TFZerosLike extends TFTensor {
+    constructor(public data: number | undefined = undefined,
+                public name: string | undefined = undefined) {
+        super(data, name);
+    }
+
+    code(storageLinks,storageNodes) {
+        let param: string = "0";
+
+        let input = this.inputs[0];
+        if(input.link!=null){
+
+            const link = storageLinks.find(element => element.id ==input.link);
+            const inputNode = storageNodes.find(element => element.id == link.origin_id);
+
+            param = inputNode.name;
+        }
+
+        return `${this.name} = tf.zerosLike(${param})`;
+    }
+
+    UIStructure(node: LGraphNode){
+        node.addInput("X","tf.Tensor");
+        node.addOutput("tf.Tensor","tf.Tensor");
+    }
+}
