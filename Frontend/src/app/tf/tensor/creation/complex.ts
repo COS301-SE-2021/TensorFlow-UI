946d40db6dce293295b9a558abc9cc643e76de55
Siviwe22 <Siviwe22@users.noreply.github.com>
2021-09-12 20:15:45 +0200

    Update: Complete implementation for TFComplex and TFDiag
     - Both the UIStructure and code functions implemented for both functions

--- Frontend/src/app/tf/tensor/creation/complex.ts
+++ Frontend/src/app/tf/tensor/creation/complex.ts
@@ -7,10 +7,32 @@ export class TFComplex extends TFTensor {
 		super(data, name);
 	}
 
-	code() {
-		return `${this.name} = tf.complex(${
-			this.data || "some value"
-		})`;
+	code(storageLinks,storageNodes) {
+
+		let param1: string = "0";
+		let param2: string = "0";
+
+		for(let i=0; i<this.inputs.length; ++i){
+			let input = this.inputs[i];
+			if(input.link!=null){
+
+				const link = storageLinks.find(element => element.id ==input.link);
+				const inputNode = storageNodes.find(element => element.id == link.origin_id);
+
+				if(i==0) {
+					param1 = inputNode.name;
+				}
+				else {
+					param2 = inputNode.name;
+				}
+			}
+		}
+		this.returnValue = param1+","+param2;
+		return `${this.name} = tf.complex(${param1},${param2})`;
 	}
-	UIStructure(node: LGraphNode){}
+	UIStructure(node: LGraphNode){
+		node.addInput("Real", "tf.Tensor"); //should be tf.Tensor|TypedArray|Array
+		node.addInput("Imag", "tf.Tensor"); //should be tf.Tensor|TypedArray|Array
+		node.addOutput("Complex", "tf.Tensor");
+	}
 }
