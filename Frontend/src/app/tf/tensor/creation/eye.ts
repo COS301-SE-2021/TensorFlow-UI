20f3d354a50a41af842b88182d39ba6f114d9edf
Siviwe22 <Siviwe22@users.noreply.github.com>
2021-09-12 20:04:08 +0200

    Update: complete implementation for TFEye class
     - isBatchShapeCorrect fucntion created to test if the specified shape component is correct.
     - code and UIStructure functions implemented

--- Frontend/src/app/tf/tensor/creation/eye.ts
+++ Frontend/src/app/tf/tensor/creation/eye.ts
@@ -1,5 +1,6 @@
 import {TFTensor} from "../tensor";
 import {LGraphNode} from "litegraph.js";
+import {NavbarComponent} from "../../../Components/navbar/navbar.component";
 
 export class TFEye extends TFTensor {
 	constructor(public data: number | undefined = undefined,
@@ -8,9 +9,136 @@ export class TFEye extends TFTensor {
 	}
 
 	code() {
+		let result: string = "";
+
+		let numOfRows = this.widgets.find(element => element.type == "numRows");
+		if(numOfRows==undefined)
+			result = "1";
+		else
+			result = numOfRows.value;
+
+		let numColumns = this.widgets.find(element => element.type == "numColumns");
+		if(numColumns!=undefined) {
+			result+=","+numColumns.value;
+		}
+
+		let batchShape = this.widgets.find(element => element.type == "batchShape");
+		if(batchShape!=undefined) {
+			result+=","+batchShape.value;
+		}
+
+		let dType = this.widgets.find(element => element.type == "dtype");
+		if(dType!=undefined) {
+			result+=","+dType.value;
+		}
+		this.returnValue = "tf.Tensor2D";
+
 		return `${this.name} = tf.eye(${
-			this.data || "some value"
+			result
 		})`;
 	}
-	UIStructure(node: LGraphNode){}
+
+	UIStructure(node: LGraphNode,navbar?:NavbarComponent) {
+		const that = this;
+
+		let widgetsData= ["1","0","[0]","float32"];
+		let widgetTypes=["numRows","numColumns","batchShape","dtype"];
+
+		for(let i=0; i<4;++i){
+			let widget = this.widgets.find(element => element.type === widgetTypes[i]);
+			if(widget!=null)
+				widgetsData[i] = widget.value;
+		}
+
+		node.addWidget("text","numRows",widgetsData[0],function (value){
+			value = value.trim();
+			if(that.checkIfNumber(value))
+				that.changeWidgetValue(value, widgetTypes[0], navbar);
+			else{
+				console.log("Error - 'numRows' can only be a number");
+				const link = "https://www.tensorflow.org/api_docs/python/tf/eye?hl=zh-cn";
+				//TODO - Throw error
+			}
+		});
+		node.addWidget("text","numColumns(optional)",widgetsData[1],function (value){
+			value = value.trim();
+			if(that.checkIfNumber(value))
+				that.changeWidgetValue(value, widgetTypes[0], navbar);
+			else{
+				console.log("Error - 'numColumns' can only be a number");
+				const link = "https://www.tensorflow.org/api_docs/python/tf/eye?hl=zh-cn";
+				//TODO - Throw error
+			}
+		});
+		node.addWidget("text","batchShape(optional)",widgetsData[2],function (value){
+			value = value.trim();
+			if(that.isBatchShapeCorrect(value))
+				that.changeWidgetValue(value,widgetTypes[2],navbar)
+		});
+		node.addWidget("combo","dtype(optional)",widgetsData[3],function (value){
+			that.changeWidgetValue(value,widgetTypes[3],navbar);
+		},{values: ["float","float32","int32","bool","complex64","string"]});
+
+		node.addOutput("tf.Tensor2D","tf.Tensor");
+	}
+
+	isBatchShapeCorrect(batchString: string): boolean{
+		let errorMessage = "batchShape has to be either [ number ]|[number, number]|[number, number, number]|[number, number, number, number], format inputted is incorrect";
+		if(batchString.charAt(0)!=="["){
+			console.log(errorMessage)
+			return false;
+		}
+		if(batchString.charAt(batchString.length-1)!==']'){
+			console.log(errorMessage)
+			return false;
+		}
+		batchString = batchString.substring(1);
+		batchString = batchString.substring(0,batchString.length-1);
+		if(batchString.length==0){
+			console.log(errorMessage);
+			return false;
+		}
+
+		for(let i=0; i<4;++i){
+
+			let endBracketIndex = batchString.indexOf(',');
+			if(endBracketIndex>-1 && batchString.length>0){
+				let numberToTest = batchString.substring(0,endBracketIndex);
+				if(!this.checkIfNumber(numberToTest)){
+					console.log(errorMessage);
+					return false;
+				}
+
+				batchString = batchString.substring(endBracketIndex);
+				if(batchString.length>1 && batchString.charAt(0)==','){
+					batchString = batchString.substring(1);
+					if(!this.checkIfNumber(numberToTest)){
+						console.log(errorMessage);
+						return false;
+					}
+				}
+				else if(batchString.length==1 && batchString.charAt(0)==','){
+					let errorMessage2 = "batchShape has to be either [ number ]|[number, number]|[number, number, number]|[number, number, number, number], format inputted is incorrect. Size too long";
+					console.log(errorMessage2);
+					return false;
+				}
+			}
+			else{
+				let numberToTest = batchString;
+				if(!this.checkIfNumber(numberToTest)){
+					console.log(errorMessage);
+					return false;
+				}
+				return true;
+			}
+		}
+		if(batchString.length>0){
+			let errorMessage2 = "batchShape has to be either [ number ]|[number, number]|[number, number, number]|[number, number, number, number], format inputted is incorrect. Size too long";
+			console.log(errorMessage2);
+			return false;
+		}
+		else{
+			return true;
+		}
+	}
 }
