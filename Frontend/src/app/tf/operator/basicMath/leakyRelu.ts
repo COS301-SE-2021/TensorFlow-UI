0d64938d3663879ebd921d16f450197d87db3957
Siviwe22 <Siviwe22@users.noreply.github.com>
2021-09-14 15:49:55 +0200

    Update: code and UIStructure functions fully implemented for TFStep, TFPrelu and TFLeakyRely

--- Frontend/src/app/tf/operator/basicMath/leakyRelu.ts
+++ Frontend/src/app/tf/operator/basicMath/leakyRelu.ts
@@ -1,5 +1,6 @@
 import {TFOperator} from "../operator";
 import {LGraphNode} from "litegraph.js";
+import {NavbarComponent} from "../../../Components/navbar/navbar.component";
 
 export class TFLeakyRelu extends TFOperator{
 	constructor(
@@ -8,12 +9,39 @@ export class TFLeakyRelu extends TFOperator{
 	}
 
 	code(storageLinks,storageNodes) {
-		return `${this.name} = tf.math.leakyRelu(
-		${this.GetNode(storageLinks, storageNodes, this.inputs[0].link)})`;
+
+		let param1: string="";
+		if(this.inputs!=null){
+			param1 = this.GetNode(storageLinks,storageNodes,this.inputs[0].link);
+		}
+		let alpha = this.widgets.find(element => element.type == "alpha")?.value;
+		if(!alpha){
+			alpha="0";
+		}
+
+		return `${this.name} = tf.nn.leaky_relu(${param1},${alpha})`;
 	}
 
-	UIStructure(node: LGraphNode) {
-		node.addInput("x", "tf.Tensor"); //should be tf.Tensor|TypedArray|Array
+	UIStructure(node: LGraphNode,navbar?:NavbarComponent) {
+		node.addInput("x", "tf.Tensor");
+		const that = this;
+
+		let widgetsData= ["0.2"];
+		let widgetTypes=["alpha"];
+
+		for(let i=0; i<1;++i){
+			let widget = this.widgets.find(element => element.type === widgetTypes[i]);
+			if(widget!=null)
+				widgetsData[i] = widget.value;
+		}
+
+		node.addWidget("text","alpha",widgetsData[0],function (value){
+			if(that.checkIfNumber(value))
+				that.changeWidgetValue(value,widgetTypes[0],navbar);
+			else{
+				alert("The scaling factor has to be a number");
+			}
+		});
 		node.addOutput("leakyRelu", "tf.Tensor");
 	}
 }
--- Frontend/src/app/tf/operator/basicMath/prelu.ts
+++ Frontend/src/app/tf/operator/basicMath/prelu.ts
@@ -1,5 +1,6 @@
 import {TFOperator} from "../operator";
 import {LGraphNode} from "litegraph.js";
+import {NavbarComponent} from "../../../Components/navbar/navbar.component";
 
 export class TFPrelu extends TFOperator{
 	constructor(
@@ -8,12 +9,31 @@ export class TFPrelu extends TFOperator{
 	}
 
 	code(storageLinks,storageNodes) {
-		return `${this.name} = tf.prelu(
-		${this.GetNode(storageLinks, storageNodes, this.inputs[0].link)})`;
+		return `${this.name + "= tf.math.negative("+
+		this.GetNode(storageLinks, storageNodes, this.inputs[0].link)})`;
 	}
 
-	UIStructure(node: LGraphNode) {
+	UIStructure(node: LGraphNode,navbar?:NavbarComponent) {
+		const that = this;
 		node.addInput("x", "tf.Tensor"); //should be tf.Tensor|TypedArray|Array
-		node.addOutput("prelu", "tf.Tensor");
+
+		let widgetsData= ["0.2"];
+		let widgetTypes=["alpha"];
+
+		for(let i=0; i<1;++i){
+			let widget = this.widgets.find(element => element.type === widgetTypes[i]);
+			if(widget!=null)
+				widgetsData[i] = widget.value;
+		}
+
+		node.addWidget("text","alpha",widgetsData[0],function (value){
+			if(that.checkIfNumber(value))
+				that.changeWidgetValue(value,widgetTypes[0],navbar);
+			else{
+				alert("The scaling factor has to be a number");
+			}
+		});
+
+		node.addOutput("x<0 ? alpha*x : f(x)=x", "tf.Tensor");
 	}
 }
