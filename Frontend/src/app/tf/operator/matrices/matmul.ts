546150ba8d097eda6d3d8c5966472ce258d52cdf
Siviwe22 <Siviwe22@users.noreply.github.com>
2021-09-15 12:16:50 +0200

    Revert "Update: Full implementation of the matmul and transpose classes"
    This reverts commit 6a1de191ff1c1577b3aae6ab87ffaac84bf2f46d.

--- Frontend/src/app/tf/operator/matrices/matmul.ts
+++ Frontend/src/app/tf/operator/matrices/matmul.ts
@@ -1,7 +1,5 @@
 import {TFOperator} from "../operator";
 import {LGraphNode} from "litegraph.js";
-import {NavbarComponent} from "../../../Components/navbar/navbar.component";
-import {TFNode} from "../../node";
 
 export class TFMatMul extends TFOperator {
 
@@ -11,80 +9,23 @@ export class TFMatMul extends TFOperator {
 	}
 
 	code(storageLinks, storageNodes) {
-
-		let parameters: TFNode[] = [];
-		let dTypeArray: string[] = [];
-
-		for(let input of this.inputs){
-			const link = storageLinks.find(element => element.id == input.link);
-			const inputNode = storageNodes.find(element => element.id == link?.origin_id);
-			parameters.push(inputNode);
-
-			if(inputNode){
-				for(let widget of inputNode.widgets){
-					if(widget.type === "dtype"){
-						dTypeArray.push( widget.value);
-						break;
-					}
-				}
-			}
-			else {
-				alert("Both input nodes (t1 and t2) are required");
-				return;
-			}
-		}
-
-		if((dTypeArray[0] !== "float32") && (dTypeArray[0] !== "float64")){
-			alert("Input t1 must be of type float32 or float64");
-			return;
-		}
-		else if(dTypeArray[0] !== dTypeArray[1]){
-			alert("Input t2 must have the same dtype as t1");
-			return;
-		}
-
-		let result="";
-		result = <string>parameters[0].name+","+parameters[1].name;
-		let widgetTypes= ["transpose_a","transpose_b","adjoint_a","adjoint_b"];
-		let widgetsSearch = this.widgets.find(element => element.type === widgetTypes[0] || widgetTypes[1] || widgetTypes[2] || widgetTypes[3]);
-		if(widgetsSearch!=null){
-			for(let i=0; i<4;++i){
-				let widget = this.widgets.find(element => element.type === widgetTypes[i])?.value;
-				if(widget!=undefined){
-					result+=","+widget;
-				}
-				else{
-					result+=","+"false";
-				}
-			}
-		}
-
-		return `${this.name + " = tf.matmul("
-			+result+")"
-		}`;
-
+		return `${this.name + "= tf.matMul(" +
+			this.GetNode(storageLinks, storageNodes, this.inputs[0].link) + "," +
+			this.GetNode(storageLinks, storageNodes, this.inputs[1].link) + "," +
+			this.widgets.find(element => element.type == "transposeA")?.value || "" + "," +
+			this.widgets.find(element => element.type == "transposeB")?.value || ""}
+		})`;
 	}
 
-	UIStructure(node: LGraphNode,navbar?:NavbarComponent) {
-		const that =this;
-		node.addInput("t1", "tf.Tensor");
-		node.addInput("t2", "tf.Tensor");
-
-		let widgetsData=[false,false,false,false];
-		let widgetTypes= ["transpose_a","transpose_b","adjoint_a","adjoint_b"];
-
-		for(let i=0; i<4;++i){
-			let widget = this.widgets.find(element => element.type === widgetTypes[i]);
-			if(widget!=null)
-				widgetsData[i] = Boolean(widget.value);
-		}
-
-		for(let i=0; i<4;++i){
-			node.addWidget("toggle",widgetTypes[i],widgetsData[i],function (value){
-				that.changeWidgetValue(value,widgetTypes[i],navbar);
-			});
-		}
-
-		node.addOutput("t1.t2", "tf.Tensor");
+	UIStructure(node: LGraphNode) {
+		node.addInput("A", "tf.Tensor"); //should be tf.Tensor|TypedArray|Array
+		node.addInput("B", "tf.Tensor"); //should be tf.Tensor|TypedArray|Array
+		node.addWidget("toggle", "transposeA", false,  (value) => {
+			this.changeWidgetValue(value, "transposeA")
+		}, {values: [true, false]});
+		node.addWidget("toggle", "transposeB", false,  (value) => {
+			this.changeWidgetValue(value, "transposeB")
+		}, {values: [true, false]});
+		node.addOutput("A*B", "tf.Tensor");
 	}
 }
--- Frontend/src/app/tf/operator/matrices/transpose.ts
+++ Frontend/src/app/tf/operator/matrices/transpose.ts
@@ -1,6 +1,5 @@
 import {TFOperator} from "../operator";
 import {LGraphNode} from "litegraph.js";
-import {NavbarComponent} from "../../../Components/navbar/navbar.component";
 
 export class TFTranspose extends TFOperator {
 
@@ -10,60 +9,16 @@ export class TFTranspose extends TFOperator {
 	}
 
 	code(storageLinks, storageNodes){
-		return `${this.name + "tf.transpose("+
-			this.GetNode(storageLinks, storageNodes, this.inputs[0].link)+","+
-			this.GetNode(storageLinks, storageNodes, this.inputs[1].link)+")"
+		return `${this.name} = tf.transpose(
+		${this.GetNode(storageLinks, storageNodes, this.inputs[0].link)},
+		${this.widgets.find(element => element.type == "perm")?.value || ""}
 		})`;
 	}
 
-	UIStructure(node: LGraphNode,navbar?:NavbarComponent) {
-		const that=this;
-		node.addInput("x", "tf.Tensor");
-
-		let widgetsData=["[1]",false];
-		let widgetTypes= ["perm?","conjugate"];
-
-		for(let i=0; i<2;++i){
-			let widget = this.widgets.find(element => element.type === widgetTypes[i]);
-			if(widget!=null) {
-				widgetsData[i] = widget.value;
-			}
-		}
-
-		node.addWidget("text",widgetTypes[0],widgetsData[0], function (value) {
-			if(that.checkIfPermIsVectorArray(value))
-				that.changeWidgetValue(value, widgetTypes[0],navbar);
-		});
-		node.addWidget("toggle",widgetTypes[1],widgetsData[1],function (value){
-			that.changeWidgetValue(value,widgetTypes[1],navbar);
-		});
+	UIStructure(node: LGraphNode) {
+		node.addInput("x", "tf.Tensor"); //should be tf.Tensor|TypedArray|Array
+		node.addWidget("text","perm?","", (value) => { this.changeWidgetValue(value, "perm")});
 		node.addOutput("tf.Tensor", "tf.Tensor");
-	}
-
-	checkIfPermIsVectorArray(value:string):boolean{
-		value.trim();
-
-		if(value.length>1){
-			if(value.charAt(0)!=='[' || value.charAt(value.length-1)!==']'){
-				alert("The permutation of the dimensions of x, has to be a vector array of type number[]")
-				return false;
-			}
-			else{
-				value = value.substring(1,value.length-1);
-				let newVal = value.split(',');
-				for(let elem of newVal){
-					if(isNaN(Number(elem))){
-						alert("The permutation of the dimensions of x, has to be a vector array of type number[]")
-						return false;
-					}
-				}
-			}
-		}
-		else{
-			alert("The permutation of the dimensions of x, has to be a vector array of type number[]")
-			return false;
-		}
-		return true;
 	}
 
 }
