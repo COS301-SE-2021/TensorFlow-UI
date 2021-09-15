898328ca716c6b474ffcd4d2bd51673715151322
Siviwe22 <Siviwe22@users.noreply.github.com>
2021-09-15 12:17:31 +0200

    Revert "Revert "Update: Full implementation of the matmul and transpose classes""
    This reverts commit 546150ba8d097eda6d3d8c5966472ce258d52cdf.

--- Frontend/src/app/tf/operator/matrices/transpose.ts
+++ Frontend/src/app/tf/operator/matrices/transpose.ts
@@ -1,5 +1,6 @@
 import {TFOperator} from "../operator";
 import {LGraphNode} from "litegraph.js";
+import {NavbarComponent} from "../../../Components/navbar/navbar.component";
 
 export class TFTranspose extends TFOperator {
 
@@ -9,16 +10,60 @@ export class TFTranspose extends TFOperator {
 	}
 
 	code(storageLinks, storageNodes){
-		return `${this.name} = tf.transpose(
-		${this.GetNode(storageLinks, storageNodes, this.inputs[0].link)},
-		${this.widgets.find(element => element.type == "perm")?.value || ""}
+		return `${this.name + "tf.transpose("+
+			this.GetNode(storageLinks, storageNodes, this.inputs[0].link)+","+
+			this.GetNode(storageLinks, storageNodes, this.inputs[1].link)+")"
 		})`;
 	}
 
-	UIStructure(node: LGraphNode) {
-		node.addInput("x", "tf.Tensor"); //should be tf.Tensor|TypedArray|Array
-		node.addWidget("text","perm?","", (value) => { this.changeWidgetValue(value, "perm")});
+	UIStructure(node: LGraphNode,navbar?:NavbarComponent) {
+		const that=this;
+		node.addInput("x", "tf.Tensor");
+
+		let widgetsData=["[1]",false];
+		let widgetTypes= ["perm?","conjugate"];
+
+		for(let i=0; i<2;++i){
+			let widget = this.widgets.find(element => element.type === widgetTypes[i]);
+			if(widget!=null) {
+				widgetsData[i] = widget.value;
+			}
+		}
+
+		node.addWidget("text",widgetTypes[0],widgetsData[0], function (value) {
+			if(that.checkIfPermIsVectorArray(value))
+				that.changeWidgetValue(value, widgetTypes[0],navbar);
+		});
+		node.addWidget("toggle",widgetTypes[1],widgetsData[1],function (value){
+			that.changeWidgetValue(value,widgetTypes[1],navbar);
+		});
 		node.addOutput("tf.Tensor", "tf.Tensor");
+	}
+
+	checkIfPermIsVectorArray(value:string):boolean{
+		value.trim();
+
+		if(value.length>1){
+			if(value.charAt(0)!=='[' || value.charAt(value.length-1)!==']'){
+				alert("The permutation of the dimensions of x, has to be a vector array of type number[]")
+				return false;
+			}
+			else{
+				value = value.substring(1,value.length-1);
+				let newVal = value.split(',');
+				for(let elem of newVal){
+					if(isNaN(Number(elem))){
+						alert("The permutation of the dimensions of x, has to be a vector array of type number[]")
+						return false;
+					}
+				}
+			}
+		}
+		else{
+			alert("The permutation of the dimensions of x, has to be a vector array of type number[]")
+			return false;
+		}
+		return true;
 	}
 
 }
