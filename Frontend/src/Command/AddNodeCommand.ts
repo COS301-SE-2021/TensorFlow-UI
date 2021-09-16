0638c99ff4a1384e82817301f77a866e239d825b
FelipeJorge805 <feliperkj805@gmail.com>
2021-09-16 12:38:49 +0200

    Add: added functionality to the AddNodeCommand class with helper functions.
    The execute() method has the same functionality as the old createComponent() method from navbar.
    The getNode() method will return the last node created.
    The setComponent() method needs to be called before execute() in order to create the proper node type.

--- Frontend/src/Command/AddNodeCommand.ts
+++ Frontend/src/Command/AddNodeCommand.ts
@@ -1,14 +1,36 @@
 import {Command} from "./Command";
 import {Store} from "@ngxs/store";
+import {NavbarComponent} from "../app/Components/navbar/navbar.component";
+import {NodeStore, TFNode} from "../app/tf";
 
 export class AddNodeCommand extends Command{
+  private nav: NavbarComponent
+  private component: string;
+  private lastNodeCreated: TFNode;
 
-  constructor(store: Store) {
+  constructor(store: Store, navbar: NavbarComponent) {
     super(store);
+    this.nav = navbar;
   }
 
   execute() {
+    let tfnode: TFNode
+    let id: string = Math.random().toString(36).substr(2, 9);
+
+    tfnode = new NodeStore[this.component]();
+    tfnode.name = this.component + id;
+    const liteGraphNode = this.nav.createLiteNode(this.component, false, tfnode);
+    this.nav.createComponentSwitchDefaults(tfnode, liteGraphNode, this.component);
+    this.lastNodeCreated = tfnode;
     return true;
+  }
+
+  setComponent(c: string){
+    this.component = c;
+  }
+
+  getNode(){
+    return this.lastNodeCreated;
   }
 
   undo() {
