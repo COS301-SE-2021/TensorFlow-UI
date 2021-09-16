6731b629d5db22ed57ad6da361ca2d9ccce4e5d1
W-Kruger <u18014934@tuks.co.za>
2021-09-16 15:22:08 +0200

    Update: Added search bar functionality.

--- Frontend/src/app/ImportPage/importPageContent/import-page-content.component.ts
+++ Frontend/src/app/ImportPage/importPageContent/import-page-content.component.ts
@@ -1,7 +1,9 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {Store} from "@ngxs/store";
 import {GitAPI} from "../../git-api";
-import projectList from "../../Workspace/import/import.component";
+
+let projectList: string[] = [];
+export default projectList;
 
 @Component({
   selector: 'app-import-page-content',
@@ -10,8 +12,10 @@ import projectList from "../../Workspace/import/import.component";
   encapsulation: ViewEncapsulation.None
 })
 export class ImportPageContentComponent implements OnInit {
-
+    public projectL: string[] = [""];
   public gitAPI: GitAPI;
+  public screenWidth = screen.width;
+  public screenHeight = screen.height;
   constructor(private store: Store) { }
 
   ngOnInit(): void {
@@ -19,9 +23,49 @@ export class ImportPageContentComponent implements OnInit {
 
 
     this.gitAPI.GetList();
+    this.projectL = projectList;
     console.log(this.gitAPI);
     console.log(projectList);
   }
 
 
+    ImportFromPC() {
+
+    }
+
+    filter() {
+        let el = document.getElementById("search") as HTMLInputElement;
+        if (el){
+            let val = el.value;
+            let cards = document.getElementsByClassName("projectsListCard1");
+            if (cards){
+                for (let i = 0; i < cards.length; i++) {
+                    let cardEl = cards.item(i) as HTMLElement;
+                    if (cardEl){
+                        if(cardEl.id.toLowerCase().includes(val.toLowerCase())){
+                            cardEl.style.display = "block";
+                        } else {
+                            cardEl.style.display = "none";
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    clearSearch() {
+        let el = document.getElementById("search") as HTMLInputElement;
+        if (el){
+            el.value="";
+            let cards = document.getElementsByClassName("projectsListCard1");
+            if (cards){
+                for (let i = 0; i < cards.length; i++) {
+                    let cardEl = cards.item(i) as HTMLElement;
+                    if (cardEl){
+                        cardEl.style.display = "block";
+                    }
+                }
+            }
+        }
+    }
 }
