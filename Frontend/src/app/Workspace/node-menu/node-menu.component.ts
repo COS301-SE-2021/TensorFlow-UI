92432b5fc779835abbd4a01f0e74196327a096d5
FelipeJorge805 <feliperkj805@gmail.com>
2021-09-17 14:12:36 +0200

    Add: deleteNode functionality in node-menu

--- Frontend/src/app/Workspace/node-menu/node-menu.component.ts
+++ Frontend/src/app/Workspace/node-menu/node-menu.component.ts
@@ -1,5 +1,9 @@
 import {AfterContentInit, Component, Input, OnInit} from '@angular/core';
 import {CommonModule} from "@angular/common";
+import {NavbarComponent} from "../../Components/navbar/navbar.component";
+import {Store} from "@ngxs/store";
+import {RemoveNodeFromStorage, RemoveTFNode} from "../../../Storage/workspace";
+import {nodeDebugInfo} from "@angular/compiler-cli/src/ngtsc/util/src/typescript";
 
 @Component({
   selector: 'app-node-menu',
@@ -8,6 +12,8 @@ import {CommonModule} from "@angular/common";
 })
 export class NodeMenuComponent implements OnInit, AfterContentInit {
 
+  @Input() store;
+  @Input() navbar;
   @Input() nodeInput;
   constructor() { }
 
@@ -20,6 +26,23 @@ export class NodeMenuComponent implements OnInit, AfterContentInit {
 
   openNodeView(navbar){
 
+  }
+
+  deleteNode(){
+    let nodes = this.navbar.TFNodeList;
+    let that = this;
+    let tempNode;
+
+    //console.log(this.navbar.TFNodeList)
+    nodes.forEach(function(element){
+      if(element.id == that.nodeInput.id) {
+        that.store.dispatch(new RemoveTFNode(element))
+        tempNode = element;
+      }
+    });
+    this.navbar.graph.remove(this.nodeInput);
+    let i = this.navbar.TFNodeList.findIndex(element => tempNode.id == element.id)
+    this.navbar.TFNodeList.splice(i,1);
   }
 
   closeNodeView(){
